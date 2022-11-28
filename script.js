0e62586411ef3455e01cf169d7b3a328ee156fd5
WinserEX <winser500@gmail.com>
2022-11-27 23:14:44 -0400

    It is properly fetching name and sprite now

---
+++ script.js
@@ -0,0 +1,30 @@
+const form = document.querySelector('#form');
+
+
+form.addEventListener('submit', async (pre) => {
+    pre.preventDefault();
+    try {
+        const searchTerm = form.elements.query.value
+        const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${searchTerm}`);
+        makeComponent(res); 
+    } 
+    catch (e) {
+        alert("No Pokemon Available! Sorry :(", e);
+    }
+}) 
+
+//Selects target 
+const target = document.querySelector('#target');
+
+const makeComponent = (pokemon) => {   
+        const p = document.createElement('P');
+        target.append(p);
+        p.innerHTML = pokemon.data.name;
+        const img = document.createElement('IMG');
+        target.append(img);
+        img.src = pokemon.data.sprites.front_default;            
+}
+
+
+
+
